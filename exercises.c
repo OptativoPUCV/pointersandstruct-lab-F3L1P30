6881bd80beba7613fcf9712421977cd2ef1bf8ec
Replit user <replituser@example.com>
2021-03-17 04:51:03 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -82,6 +82,11 @@ Vector * crearVector(int n) {
    Vector *crear;
    crear = (Vector *) calloc(n , sizeof(Vector));
   crear->datos = (int *)calloc (n, sizeof(int));
+  int i;
+  for(i = 0; i <= n; i++){
+    crear->datos = 0;
+    crear->capacidad = 0;
+  }
   return crear;
 
 }
